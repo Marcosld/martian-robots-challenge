43a22845b08e7dbf9f20bd82ba0500286ed6becb
Marcos Luis Delgado <marcosluisdelgado@gmail.com>
2019-05-12 19:18:14 +0200

    Minor refactor

--- src/executor.js
+++ src/executor.js
@@ -1,13 +1,12 @@
 const actions = require('./actions')
 const { fall } = require('./effects')
 
-const getNextState = (world, position, action) => fall(world, position, actions[action](position))
+const getNextState = (world, position, action) =>
+  position.isFallen ? { world, position } : fall(world, position, actions[action](position))
 
 const executeInstructions = (world, { position, actions }) =>
   actions.reduce(({ world, position }, action) =>
-    position.isFallen
-      ? { world, position }
-      : getNextState(world, position, action)
+    getNextState(world, position, action)
   , { world, position })
 
 module.exports = {
